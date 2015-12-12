4dc1c6ed24fe8efddf4b7c67e5e9e4d047ed9436
Vincent Peng <vincentpeng007@outlook.com>
2015-12-12 19:04:56 +0800

    1.txt

--- src/com/ruanko/dao/AdminDao.java
+++
@@ -1,9 +0,0 @@
-package com.ruanko.dao;
-
-import com.ruanko.model.Admin;
-import com.ruanko.model.User;
-
-public interface AdminDao {
-	public Admin checkAdminLogin(Admin admin);
-	public User user_del(User user);
-}
